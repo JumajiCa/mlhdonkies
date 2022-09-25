7a7ee0528b1474d0f28f5632e44d3b6dafd941f4
Amen <jumajiyi@protonmail.com>
2022-09-25 04:34:46 -0700

    Ehe.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, valid_login
+from flask import Flask, render_template, request
 
 app = Flask(__name__);
 
@@ -24,19 +24,4 @@ def render():
 @app.route("/home")
 def hello_world():
     return render_template('main_page.html', the_title='pog');
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return (request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
-if __name__ == "__main__":
-    app.run(debug=True);
 
